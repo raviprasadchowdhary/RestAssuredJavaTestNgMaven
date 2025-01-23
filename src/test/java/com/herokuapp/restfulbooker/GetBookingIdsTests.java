2abf37e3152b8f552b8aff6aafe690ce0278e32f
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-01-23 22:34:55 +0530

    getBookingIdsWithoutFilter

---
+++ src/test/java/com/herokuapp/restfulbooker/GetBookingIdsTests.java
@@ -0,0 +1,25 @@
+package com.herokuapp.restfulbooker;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class GetBookingIdsTests {
+
+    @Test
+    public void getBookingIdsWithoutFilter(){
+        // Get response
+        Response response = RestAssured.get("https://restful-booker.herokuapp.com/booking");
+
+        // verify response 200
+        Assert.assertEquals(response.getStatusCode(), 200);
+
+        // verify at least 1 booking id in response
+        List <Integer> bookingidList = response.jsonPath().getList("bookingid");
+        Assert.assertFalse(bookingidList.isEmpty());
+
+    }
+}
