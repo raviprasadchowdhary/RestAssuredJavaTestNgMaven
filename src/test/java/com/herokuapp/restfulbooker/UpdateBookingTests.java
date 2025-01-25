d6ee8c7d2cdb90a9a78d46d2caa4b477628a8e0a
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-01-25 23:13:44 +0530

    updateBookingTest

---
+++ src/test/java/com/herokuapp/restfulbooker/UpdateBookingTests.java
@@ -0,0 +1,25 @@
+package com.herokuapp.restfulbooker;
+
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class UpdateBookingTests extends BaseTest{
+    SoftAssert softAssert = new SoftAssert();
+
+    @Test
+    public void updateBookingTest(){
+        Response createBookingResponse = createBooking();
+        int bookingId = createBookingResponse.jsonPath().getInt("bookingid");
+
+        Response response = updateBooking(bookingId);
+
+        softAssert.assertEquals(response.jsonPath().getString("firstname"), "Alex2");
+        softAssert.assertEquals(response.jsonPath().getString("lastname"), "Periera2");
+        softAssert.assertEquals(response.jsonPath().getInt("totalprice"), 102);
+        softAssert.assertEquals((String.valueOf(response.jsonPath().getBoolean("depositpaid"))), "false");
+        softAssert.assertEquals(response.jsonPath().getString("bookingdates.checkin"), "2025-02-01");
+        softAssert.assertEquals(response.jsonPath().getString("bookingdates.checkout"), "2025-02-10");
+        softAssert.assertEquals(response.jsonPath().getString("additionalneeds"), "Wrestling2");
+    }
+}
