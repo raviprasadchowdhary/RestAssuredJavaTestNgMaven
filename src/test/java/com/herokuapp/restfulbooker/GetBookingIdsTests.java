c8c86afd87e85f9189f402b0fa81d989f181003a
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-01-26 09:51:56 +0530

    refactor

--- src/test/java/com/herokuapp/restfulbooker/GetBookingIdsTests.java
+++ src/test/java/com/herokuapp/restfulbooker/GetBookingIdsTests.java
@@ -1,10 +1,7 @@
 package com.herokuapp.restfulbooker;
 
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestLogSpecification;
-import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
