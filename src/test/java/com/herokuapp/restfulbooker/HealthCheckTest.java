e3ab77dee83cea2f0dd6985c79837d83e73b4156
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-01-26 13:34:23 +0530

    headersAndCookiesTest

--- src/test/java/com/herokuapp/restfulbooker/HealthCheckTest.java
+++ src/test/java/com/herokuapp/restfulbooker/HealthCheckTest.java
@@ -1,5 +1,9 @@
 package com.herokuapp.restfulbooker;
 
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -14,5 +18,16 @@ public class HealthCheckTest extends BaseTest{
         then().
                 assertThat().
                 statusCode(201);
+    }
+
+    @Test
+    public void headersAndCookiesTest(){
+        Response response = given(spec).when().get("ping");
+
+        Headers headers = response.getHeaders();
+        Header server = headers.get("Server");
+
+        Assert.assertEquals(response.getStatusCode(),201);
+        Assert.assertEquals(server.getValue(), "Cowboy");
     }
 }
